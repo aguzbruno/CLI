474c14894ff8f771ea60062348fbd304a1188d56
Aguzbruno <aguzbruno@hotmail.com>
2021-05-19 15:37:30 -0300

    CartWidget

---
+++ src/components/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import '../index.css'
+
+const ItemListContainer =({})=>{
+  return (
+  <header className="Contenedor">
+      <h1>Soy el ListContainer</h1>
+  </header>
+
+
+)
+};
+export default ItemListContainer;
